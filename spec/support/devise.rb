63dca036220fe0fcb12a560b7c4a4d0abaad4c13
riggraz <riccardo.graziosi97@gmail.com>
2019-08-19 10:52:26 +0200

    Add Devise configuration for testing

---
+++ spec/support/devise.rb
@@ -0,0 +1,6 @@
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :request
+end
