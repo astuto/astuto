7cae9d68c17e21ddc3287655c4af63a9c73a1ed9
riggraz <riccardo.graziosi97@gmail.com>
2019-08-19 17:41:47 +0200

    Add a mock home page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
   devise_for :users
 end
