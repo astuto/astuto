3bd019bfdc420c84592383a8cdb2c45f10dc40cb
riggraz <riccardo.graziosi97@gmail.com>
2019-08-19 10:52:12 +0200

    Add test to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) { User.new(email: 'example@example.com', password: 'password') }
+
+  it 'creates a user with role "user" by default' do
+    expect(user.role).to eq('user')
+  end
+
 end
