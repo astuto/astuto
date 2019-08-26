cd79046071937aba2393336720ff5f3df273175f
riggraz <riccardo.graziosi97@gmail.com>
2019-08-26 18:19:47 +0200

    Roadmap now shows requested post statuses in order

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def roadmap
-    @post_statuses = PostStatus.select(:id, :name, :color)
+    @post_statuses = PostStatus
+      .select(:id, :name, :color)
+      .where(show_in_roadmap: true)
+      .order(order: :asc)
 
     @posts = Post
       .select(:id, :title, :board_id, :post_status_id, :user_id, :created_at)
