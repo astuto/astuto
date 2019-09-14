e92aa9842caa79e3b21435e366cdf3be260894fb
riggraz <riccardo.graziosi97@gmail.com>
2019-09-14 17:36:11 +0200

    Add redux-devtools

--- app/javascript/helpers/createStore.ts
+++ app/javascript/helpers/createStore.ts
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 
 import rootReducer from '../reducers/rootReducer';
 
+const composeEnhancers = composeWithDevTools({
+  trace: true,
+});
+
 const createStoreHelper = () => (
   createStore(
     rootReducer,
-    applyMiddleware(
-      thunkMiddleware,
-    ),
+    composeEnhancers(
+      applyMiddleware(thunkMiddleware)
+    )
   )
 );
 
