8d297a897e2a899d7df52ea5b26195547d87b490
riggraz <riccardo.graziosi97@gmail.com>
2019-09-15 18:46:54 +0200

    Refactor CSS pt. 2 (Properly structure stylesheets folder)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require("./bootstrap_custom")
-require("../stylesheets/application")
+require("../stylesheets/main")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
