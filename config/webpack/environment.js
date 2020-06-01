46bb13693bcfe43bd429db089203f5ddcda933c8
shinobushiva <stagesp1@gmail.com>
2020-06-01 22:04:14 +0900

    erb enabled

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const erb =  require('./loaders/erb')
 const typescript =  require('./loaders/typescript')
 const webpack = require('webpack')
 
@@ -12,4 +13,5 @@ environment.plugins.append(
   })
 )
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
