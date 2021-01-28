f2fc86a81bf2d6ad4c6f423de500f67b89fa23c9
Kevin Vinhas <kevin@vinhas.fr>
2021-01-29 00:49:27 +0100

    Add New Comment on your post notifications
    This notification is sent only to the post owner, unless this person turned off the notifications.
    A simple first step into the notifications by mail world :)
    
    The mail contains a link to user profile
    The link to the user profile is required to give an easy access to
    notifications disabling.
    
    Also having a preview for the notify_post_owner method
    
    We can `Comment.first` because it is part of the db:seeds method. So
    there should, in development, always be one.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: "notifications@example.com"
+
+  def notify_post_owner(comment:)
+    @comment = comment
+    @user = comment.post.user
+
+    mail(to: @user.email, subject: "[#{ENV.fetch('APP_NAME')}] - New comment on #{comment.post.title}")
+  end
+end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,6 @@ ENV["POSTS_PER_PAGE"] = "15"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
   # For Devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
@@ -46,6 +45,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.active_job.queue_adapter = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
