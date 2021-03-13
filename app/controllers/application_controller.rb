5409144c2fcee8d9069d5c4665deecde06dfbde8
Talha Mahmood <talha.mahmood@healthwire.pk>
2021-03-13 18:00:31 +0500

    Subdomains done!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :load_boards
 
+  def after_sign_out_path_for(resource_or_scope)
+    root_url(subdomain: false)
+  end
+
   def after_sign_in_path_for(resource)
      users_show_url(subdomain: resource.subdomain)
   end
