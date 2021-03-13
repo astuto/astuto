75c037725847afb51e176177c3d92f1299ae1352
Talha Mahmood <talha.mahmood@healthwire.pk>
2021-03-13 21:14:47 +0500

    Change!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-App::Application.config.session_store :cookie_store, :key => 'astuto_session', :domain => 'feedbackuser.herokuapp.com',tld_length: 2
+
+
+
+App::Application.config.session_store :cookie_store, :key => 'astuto_session', :domain => 'feedbackuser.herokuapp.com',tld_length: 1
