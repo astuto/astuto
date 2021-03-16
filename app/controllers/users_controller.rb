d593c49e155ac89338ee0b07312a4199a7efc53e
Talha Mahmood <talha.mahmood@healthwire.pk>
2021-03-16 20:56:36 +0500

    Fixes!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,11 @@ class UsersController < ApplicationController
   def show
     @user = User.where(:subdomain => request.subdomain).first || not_found
     @boards = @user.boards.select(:id, :name).order(order: :asc)
-    @post_statuses = PostStatus
-                       .find_roadmap
-                       .select(:id, :name, :color)
+    @post_statuses = PostStatus.find_roadmap.select(:id, :name, :color)
 
-    @posts = Post
-               .find_with_post_status_in(@post_statuses)
-               .select(:id, :title, :board_id, :post_status_id, :user_id, :created_at)
+    @posts = Post.joins(:board).where("posts.board_id IN (?)",@boards.pluck(:id))
+                 .find_with_post_status_in(@post_statuses)
+                 .select(:id, :title, :board_id, :post_status_id, :user_id, :created_at)
   end
 
 
