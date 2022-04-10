c5148147e3febf046db9b4795e4b6be4687cdca3
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2022-04-10 16:33:08 +0200

    Remove redux devtools from create store

--- app/javascript/helpers/createStore.ts
+++ app/javascript/helpers/createStore.ts
@@ -7,8 +7,7 @@ const createStoreHelper = () => (
   createStore(
     rootReducer,
     compose(
-      applyMiddleware(thunkMiddleware),
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      applyMiddleware(thunkMiddleware)
     )
   )
 );
