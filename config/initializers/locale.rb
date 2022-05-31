7e78b6438a0318d52e3843e00be07182757382cf
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2022-05-31 20:07:29 +0200

    Add devise translations

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,8 +1,5 @@
-# Where the I18n library should search for translation files
-# I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+# Configure I18n to look at subfolders too
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
 
-# Permitted locales available for the application
 I18n.available_locales = [:en, :it]
-
-# Set default locale to something other than :en
 I18n.default_locale = :en
