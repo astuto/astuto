61a9851916c02ff21f5adf348412eae4f61b81fe
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2022-06-19 17:46:01 +0200

    fix spec

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe 'comments routing', :aggregate_failures, type: :routing do
     expect(patch: '/posts/1/comments/1').to route_to(
       controller: 'comments', action: 'update', post_id: "1", id: "1"
     )
+    expect(delete: '/posts/1/comments/1').to route_to(
+      controller: 'comments', action: 'destroy', post_id: "1", id: "1"
+    )
 
     expect(get: '/posts/1/comments/1').not_to be_routable
     expect(get: '/posts/1/comments/new').not_to be_routable
     expect(get: '/posts/1/comments/1/edit').not_to be_routable
-    expect(delete: '/posts/1/comments/1').not_to be_routable
   end
 end
