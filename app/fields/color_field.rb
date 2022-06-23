24ee291d98c21609e3c4c15f44e779ae14cf15a3
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2022-06-23 15:34:12 +0200

    Remove old admin panel

--- app/fields/color_field.rb
+++
@@ -1,7 +0,0 @@
-require "administrate/field/base"
-
-class ColorField < Administrate::Field::Base
-  def to_s
-    data.to_s
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#roadmap'
   get '/roadmap', to: 'static_pages#roadmap'
-
-  namespace :admin do
-    root to: 'boards#index'
-
-    resources :boards
-    resources :comments
-    resources :posts
-    resources :post_statuses
-    resources :users
-  end
   
   devise_for :users, :controllers => { :registrations => 'registrations' }
   resources :users, only: [:index, :update]
