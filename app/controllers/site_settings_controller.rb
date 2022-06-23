8dfa665d54cc4fd7bd1cefc6a3545702bb1f6fe3
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2022-06-23 15:03:08 +0200

    block mods from accessing certain site settings pages

--- app/controllers/site_settings_controller.rb
+++ app/controllers/site_settings_controller.rb
@@ -1,7 +1,8 @@
 class SiteSettingsController < ApplicationController
   include ApplicationHelper
 
-  before_action :authenticate_admin
+  before_action :authenticate_admin, only: [:general, :boards, :post_statuses, :roadmap]
+  before_action :authenticate_power_user, only: [:users]
   
   def general
   end
