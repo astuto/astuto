26e2fa3a8f3f3380244568a603baaae2769cf147
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2023-03-19 15:25:01 +0100

    Fix post system specs

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -4,10 +4,10 @@ feature 'post', type: :system, js: true do
   let(:post) { FactoryBot.create(:post) }
   let(:mod) { FactoryBot.create(:moderator) }
 
-  let(:selectPickerBoard) { 'selectPickerBoard' }
-  let(:selectPickerStatus) { 'selectPickerStatus' }
+  let(:select_picker_board) { 'selectPickerBoard' }
+  let(:select_picker_status) { 'selectPickerStatus' }
 
-  let (:postContainer) { '.postAndCommentsContainer' }
+  let(:post_container) { '.postAndCommentsContainer' }
 
   it 'renders post title, description, board and status' do
     visit post_path(post)
@@ -24,26 +24,28 @@ feature 'post', type: :system, js: true do
     board1 = FactoryBot.create(:board)
     
     visit post_path(post)
-    within postContainer do
+    within post_container do
       expect(page).to have_content(post.board.name.upcase)
     end
     
     expect(post.board_id).not_to eq(board1.id)
 
-    within '.postFooterActions' do
+    within post_container do
+      # doesn't work: find('.editAction').click
       find('.actionLink', match: :first).click
     end
 
-    expect(page).to have_select(selectPickerBoard,
+    expect(page).to have_select(select_picker_board,
+      selected: post.board.name,
       with_options: [post.board.name, board1.name]
     )
     
-    select board1.name, from: selectPickerBoard
-    expect(page).to have_select selectPickerBoard, selected: board1.name
+    select board1.name, from: select_picker_board
+    expect(page).to have_select select_picker_board, selected: board1.name
 
     click_button 'Save'
 
-    within postContainer do
+    within post_container do
       expect(page).to have_content(board1.name.upcase)
     end
     expect(post.reload.board_id).to eq(board1.id)
@@ -55,25 +57,27 @@ feature 'post', type: :system, js: true do
     post_status1 = FactoryBot.create(:post_status)
     
     visit post_path(post)
-    within postContainer do
+    within post_container do
       expect(page).to have_content(post.post_status.name.upcase)
     end
     
     expect(post.post_status_id).not_to eq(post_status1.id)
 
-    within '.postFooterActions' do
+    within post_container do
+      # doesn't work: find('.editAction').click
       find('.actionLink', match: :first).click
     end
 
-    expect(page).to have_select(selectPickerStatus,
-      options: [post.post_status.name, post_status1.name, 'None']
+    expect(page).to have_select(select_picker_status,
+      selected: post.post_status.name,
+      with_options: [post.post_status.name, post_status1.name, 'None']
     )
 
-    select post_status1.name, from: selectPickerStatus
-    expect(page).to have_select selectPickerStatus, selected: post_status1.name
+    select post_status1.name, from: select_picker_status
+    expect(page).to have_select select_picker_status, selected: post_status1.name
 
     click_button 'Save'
-    within postContainer do
+    within post_container do
       expect(page).to have_content(post_status1.name.upcase)
     end
     expect(post.reload.post_status_id).to eq(post_status1.id)
