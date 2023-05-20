189d5abe27304b4e4fa7aa44c8b75990c10fe849
Riccardo Graziosi <31478034+riggraz@users.noreply.github.com>
2023-05-20 12:52:19 +0200

    Remove unused dependencies (#228)
    * Remove unused package.json dependencies
    * Remove unused files
    * Remove some bootstrap js dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,16 @@
 // Entry point for the build script in your package.json
 
 // turbolinks
-require("turbolinks").start();
+require('turbolinks').start();
 
 // react-rails
-var componentRequireContext = require.context("./components", true);
-var ReactRailsUJS = require("react_ujs");
+var componentRequireContext = require.context('./components', true);
+var ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(componentRequireContext);
 
 // bootstrap (js only)
-require('bootstrap/js/dist/alert');
-require('bootstrap/js/dist/button');
-require('bootstrap/js/dist/collapse');
-require('bootstrap/js/dist/dropdown');
 require('bootstrap/js/dist/util');
+require('bootstrap/js/dist/dropdown');
 
 // i18n-js translations
 require('./translations/index.js.erb');
