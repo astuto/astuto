bfa7273438391daf40357874de880a45c4338d00
Farbafe <yehyapal@gmail.com>
2023-07-20 19:42:40 +0300

    Dev(post settings): archives stale posts
    Sets a job to be run every 5 minutes.
    The job enqueues an async Sidekiq fanout worker.
    The fanour worker finds and enqueues stale posts to the executor.
    The executor archives the post and notifies folllowers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+RSpec::Matchers.define_negated_matcher :not_change, :change
+
 RSpec.configure do |config|
   ENV["RAILS_ENV"] = "test"
 
