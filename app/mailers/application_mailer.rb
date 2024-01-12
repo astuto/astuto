87b94e749f99eed98d6ac9b6e8474748e605a04a
riggraz <riccardo.graziosi97@gmail.com>
2024-01-12 17:51:37 +0100

    refactoring

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,17 +2,14 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
   helper :application
 
-  after_action :set_default_from
+  after_action :set_mail_from_for_multitenancy
 
   private
   
-    def set_default_from
+    def set_mail_from_for_multitenancy
       if Rails.application.multi_tenancy?
-        from = "#{Current.tenant_or_raise!.site_name} <notifications@astuto.io>"
-      else
-        from = ENV.fetch("EMAIL_MAIL_FROM", "Astuto <notifications@astuto.io>")
+        from = "#{Current.tenant_or_raise!.site_name} <#{ENV.fetch('EMAIL_MAIL_FROM', 'notifications@astuto.io')}>"
+        mail.from = from
       end
-
-      mail.from = from
     end
 end
