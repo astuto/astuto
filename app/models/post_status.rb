78c6339c733c270d6be81b17210d7a5c664ae5d0
riggraz <riccardo.graziosi97@gmail.com>
2024-01-18 16:44:02 +0100

    Destroy related post status changes when destroying a post status

--- app/models/post_status.rb
+++ app/models/post_status.rb
@@ -3,6 +3,7 @@ class PostStatus < ApplicationRecord
   include Orderable
 
   has_many :posts, dependent: :nullify
+  has_many :post_status_changes, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :tenant_id }
   validates :color, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/ }
