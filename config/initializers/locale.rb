437262040a1a635f227ce44796f327b8eb43e20a
Riccardo Graziosi <31478034+riggraz@users.noreply.github.com>
2024-03-15 00:14:31 +0100

    Add 🇨🇳 Chinese simplified (#310)
    Thanks to omltcat for the translation!

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,11 +1,12 @@
 # Configure I18n to look at subfolders too
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
 
-I18n.available_locales = [:en, :it, :de, :fr, :ru, :es, :vi]
+I18n.available_locales = [:en, :it, :de, :fr, :es, 'zh-CN', :ru, :vi]
 
 I18n.default_locale = :en
 
 # Custom pluralization rules
 # Those must be mirrored in app/javascript/translations/index.js.erb
 I18n::Backend::Simple.include(I18n::Backend::Pluralization)
+I18n.backend.store_translations 'zh-CN', i18n: { plural: { rule: lambda { |n| :other } } } # Chinese simplified
 I18n.backend.store_translations :vi, i18n: { plural: { rule: lambda { |n| :other } } } # Vietnamese
