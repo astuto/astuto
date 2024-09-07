608fc678c1edce0620103575c5e1df5f5f279b33
riggraz <riccardo.graziosi97@gmail.com>
2024-09-07 17:54:34 +0200

    Increase full_name max length to 64 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   after_initialize :set_default_role, if: :new_record?
   after_initialize :set_default_status, if: :new_record?
 
-  validates :full_name, presence: true, length: { in: 2..32 }
+  validates :full_name, presence: true, length: { in: 2..64 }
   validates :email,
     presence: true,
     uniqueness: { scope: :tenant_id, case_sensitive: false },
