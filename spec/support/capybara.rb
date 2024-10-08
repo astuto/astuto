69ac627a574403667aa8570267403876c6350808
riggraz <riccardo.graziosi97@gmail.com>
2024-10-08 12:05:11 +0200

    ⬆️

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,7 @@ Capybara.register_driver :chrome_headless do |app|
 
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
+  # options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
