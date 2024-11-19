7683c76e182c52371830a279887663208bf4b035
riggraz <riccardo.graziosi97@gmail.com>
2024-11-19 17:11:11 +0100

    Update sidekiq_cron settings

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,8 +1,12 @@
 Sidekiq::Cron.configure do |config|
-  config.cron_poll_interval = 15
   config.cron_schedule_file = 'config/sidekiq_cron_schedule.yml'
+  
+  config.cron_poll_interval = 30
   config.cron_history_size = 50
   config.default_namespace = 'default'
   config.natural_cron_parsing_mode = :strict
-  config.reschedule_grace_period = 300 # Default is 60
+  
+  # Handles the case when the Sidekiq process was down for a while and the cron job should have run (set to 10 minutes, i.e. 600 seconds)
+  # This could happen during the deployment of a new version of the application
+  config.reschedule_grace_period = 600
 end
