1cc9adc08287c9e8793db579cbf306942caa7c62
Роман Гудков <39859935+Rivega42@users.noreply.github.com>
2025-03-22 14:25:57 +0000

    Main обновлен файл ru.yml

--- app/workflows/create_welcome_entities_workflow.rb
+++ app/workflows/create_welcome_entities_workflow.rb
@@ -17,34 +17,34 @@ class CreateWelcomeEntitiesWorkflow
 
     # Create some Post Statuses
     planned_post_status = PostStatus.create!(
-      name: 'Planned',
-      color: '#0096ff',
+      name: 'Запланировано',
+      color: '#116bb2',
       order: 0,
       show_in_roadmap: true
     )
     in_progress_post_status = PostStatus.create!(
-      name: 'In Progress',
-      color: '#9437ff',
+      name: 'Выполняется',
+      color: '#685a94',
       order: 1,
       show_in_roadmap: true
     )
     completed_post_status = PostStatus.create!(
-      name: 'Completed',
-      color: '#6ac47c',
+      name: 'Завершено',
+      color: '#49b04d',
       order: 2,
       show_in_roadmap: true
     )
     rejected_post_status = PostStatus.create!(
-      name: 'Rejected',
-      color: '#ff2600',
+      name: 'Отклонено',
+      color: '#fd9e36',
       order: 3,
       show_in_roadmap: false
     )
 
     # Create some Posts
     post1 = Post.create!(
-      title: 'This is an example feedback post, click to learn more!',
-      description: 'Users can submit feedback by publishing posts like this. You can assign a **status** to each post: this one, for example, is marked as "Planned". Remember that you can customise post statuses from Site settings > Statuses',
+      title: 'Это пример поста с отзывом. Нажмите, чтобы узнать больше!',
+      description: 'Пользователи могут отправлять отзывы, публикуя посты, подобные этому. Вы можете назначить **статус** каждому посту: этот, например, отмечен как «Запланировано». Помните, что вы можете настраивать статусы постов в разделе Настройки сайта > Статусы',
       board_id: feature_board.id,
       user_id: owner.id,
       post_status_id: planned_post_status.id
@@ -56,15 +56,15 @@ class CreateWelcomeEntitiesWorkflow
     )
 
     post2 = Post.create!(
-      title: 'There are multiple boards',
-      description: 'We created two boards for you, "Feature Requests" and "Bug Reports", but you can add or remove as many as you want! Just head to Site settings > Boards!',
+      title: 'Есть несколько досок',
+      description: 'Мы создали для вас две доски: «Запросы функций» и «Отчеты об ошибках», но вы можете добавлять или удалять столько, сколько захотите! Просто перейдите в Настройки сайта > Доски!',
       board_id: bug_board.id,
       user_id: owner.id
     )
 
     # Create some comments
     post1.comments.create!(
-      body: 'Users can comment to express their opinions! As with posts and board descriptions, comments can be *Markdown* **formatted**',
+      body: 'Пользователи могут комментировать, чтобы выразить свое мнение! Как и в случае с сообщениями и описаниями досок, комментарии могут быть *Markdown* **отформатированы**',
       user_id: owner.id
     )
 
